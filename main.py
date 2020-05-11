3d1a4b17d703374638c48114a6282e768f2907a1
patrick__ <patrick.fodor.sc@gmail.com>
2020-05-11 20:57:59 +0200

    add logging output

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import uuid
+
 from flask import Flask, render_template, request, redirect, url_for, make_response
 import random
 import datetime
-from models import Users, db
+from models import Users, SecretNumberStore, db
 
 app = Flask(__name__)
 db.create_all()
+
+COOKIE_ID_STRING = "lucky_number/secret_number_identifier"
+
 
 @app.route("/")
 def main():
@@ -24,27 +29,47 @@ def about():
 @app.route("/lucky_number", methods=['GET', 'POST'])
 def lucky_number():
     if request.method == "GET":
-        secret_number = request.cookies.get("lucky_number/secret")
-        if not secret_number:
+
+        secret_number_identifier = request.cookies.get(COOKIE_ID_STRING)
+        secret_number_store = db.query(SecretNumberStore).filter_by(cookie_identifier=secret_number_identifier).first()
+
+        if not secret_number_store:
             secret_number = random.randint(1, 10)
+            cookie_identifier = str(uuid.uuid4())
+            secret_number_store = SecretNumberStore(
+                cookie_identifier=cookie_identifier,
+                secret_number=secret_number
+            )
+            db.add(secret_number_store)
+            db.commit()
 
         context = {
             "date_of_number": datetime.datetime.now().isoformat(),
         }
 
         response = make_response(render_template("lucky_number.html", **context))
-        response.set_cookie("lucky_number/secret", str(secret_number))
-
+        response.set_cookie(COOKIE_ID_STRING, str(secret_number_store.cookie_identifier))
         return response
 
     elif request.method == "POST":
         user_guess = request.form.get('number')
-        user_secret_cookie = request.cookies.get("lucky_number/secret")
-        if user_guess == user_secret_cookie:
+        cookie_identifier = request.cookies.get(COOKIE_ID_STRING)
+
+        secret_number_store = db.query(SecretNumberStore).filter_by(cookie_identifier=cookie_identifier).first()
+
+        app.logger.info(f"Secret Number Guess: user guess: {user_guess}, cookie identifier: {cookie_identifier}")
+
+        if secret_number_store and (int(user_guess) == secret_number_store.secret_number):
             response = make_response(redirect(url_for('lucky_number_success')))
-            response.set_cookie("lucky_number/secret", expires=0)
+            response.set_cookie(COOKIE_ID_STRING, expires=0)
+
+            db.delete(secret_number_store)
+            db.commit()
+            app.logger.info(f"User guessed number correctly, removing number with identifier: {cookie_identifier}")
             return response
         else:
+            app.logger.info(f"User guessed number incorrectly, redirecting to guessing page")
+            app.logger.error(f"ERROR: bad guess! do not repeat!")
             return redirect(url_for('lucky_number'))
 
 
--- models.py
+++ models.py
@@ -1,10 +1,16 @@
 import os
 from sqla_wrapper import SQLAlchemy
 
-db = SQLAlchemy(os.getenv("DATABASE_URL", "sqlite:///:memory:"))
+db = SQLAlchemy(os.getenv("DATABASE_URL", "postgres://zubkimxqawbbud:b66c2693669d89633f65a355a0af1b29595a3634c4ac96c534df6f86f2410c6e@ec2-54-247-89-181.eu-west-1.compute.amazonaws.com:5432/dejr4kmdk15a8v"))
 
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
     secret_number = db.Column(db.Integer, nullable=True)
+
+
+class SecretNumberStore(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cookie_identifier = db.Column(db.String, unique=True, nullable=False)
+    secret_number = db.Column(db.Integer, nullable=False)
