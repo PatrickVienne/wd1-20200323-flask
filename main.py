39676b7be29a5ae8c22edcfb1b46b358a4c7d1e2
patrick__ <patrick.fodor.sc@gmail.com>
2020-05-11 19:00:11 +0200

    lucky number game with cookies

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, make_response
 import random
 import datetime
 
@@ -23,15 +23,39 @@ def about():
 @app.route("/lucky_number", methods=['GET', 'POST'])
 def lucky_number():
     if request.method == "GET":
+        secret_number = request.cookies.get("lucky_number/secret")
+        if not secret_number:
+            secret_number = random.randint(1, 10)
+
         context = {
             "date_of_number": datetime.datetime.now().isoformat(),
-            "lucky_number": random.randint(1, 10)
         }
 
-        return render_template("lucky_number.html", **context)
+        response = make_response(render_template("lucky_number.html", **context))
+        response.set_cookie("lucky_number/secret", str(secret_number))
+
+        return response
 
     elif request.method == "POST":
-        return redirect(url_for('lucky_number'))
+        user_guess = request.form.get('number')
+        user_secret_cookie = request.cookies.get("lucky_number/secret")
+        if user_guess == user_secret_cookie:
+            response = make_response(redirect(url_for('lucky_number_success')))
+            response.set_cookie("lucky_number/secret", expires=0)
+            return response
+        else:
+            return redirect(url_for('lucky_number'))
+
+
+@app.route("/lucky_number/success", methods=["GET"])
+def lucky_number_success():
+    return render_template("lucky_number_success.html")
+
+
+# TODO TODAY:
+# - Post Form finish, get input parameters from request
+# - Cookies
+# - Database
 
 
 # TODO: 1) add boogle site
